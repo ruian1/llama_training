dbde5d9234cf5fc938cc27eeedd3f5935cfc730f
Rui An <anruing@gmail.com>
2023-04-03 22:41:10 +0000

    config for llama-peft

--- train.py
+++ train.py
@@ -1,3 +1,4 @@
+import json
 import os
 import pickle
 from time import time
@@ -8,13 +9,28 @@ import torch.nn as nn
 from peft import LoraConfig, TaskType, get_peft_config, get_peft_model
 from sklearn.model_selection import train_test_split
 from torch.utils.data import DataLoader, Dataset, RandomSampler
-from transformers import (AdamW,
-                          LlamaTokenizer, Trainer, TrainingArguments,
+from transformers import (AdamW, LlamaTokenizer, Trainer, TrainingArguments,
                           get_linear_schedule_with_warmup)
 
-from rui_model_debug import LlamaForSequenceClassification
-
 from llama_dataloader import BinaryClassificationDataset
+from llama_model_debug import LlamaForSequenceClassification
+
+with open('config/config_peft.json', 'r') as f:
+    config = json.load(f)
+
+print(config)
+
+max_sql_len = config['model']['max_sql_len']
+
+batch_size = config['training']['batch_size']
+epochs = config['training']['epochs']
+fp16 = config['training']['fp16']
+learning_rate = config['training']['learning_rate']
+load_in_8bit = config['training']['load_in_8bit']
+max_steps = config['training']['max_steps']
+warmup_steps = config['training']['warmup_steps']
+
+training_data_file = config['data']['training_data_file']
 
 os.environ["WANDB_MODE"] = "dryrun"
 # os.environ["CUDA_VISIBLE_DEVICES"] = "0"
@@ -22,13 +38,11 @@ os.environ["WANDB_MODE"] = "dryrun"
 
 device = "cuda" if torch.cuda.is_available() else "cpu"
 # device = "cpu"
-print(device)
+print(f"Training with {device}")
 
 FOLDER_PATH = os.getcwd()
 
 model_path = os.path.join(FOLDER_PATH, "weights/llama-7b")
-
-load_in_8bit = False
 
 tokenizer = LlamaTokenizer.from_pretrained(model_path)
 if load_in_8bit:
@@ -51,14 +65,14 @@ if not load_in_8bit:
     model = get_peft_model(model, peft_config)
     model.print_trainable_parameters()
 
-    # model = model.half()
+    model = model.half()
     model = model.to(device)
 
 print("FOLDER_PATH: ", FOLDER_PATH)
 
 directory = os.path.join(FOLDER_PATH, "data")
 training_dir = os.path.join(FOLDER_PATH, "training")
-train_df = pd.read_csv(os.path.join(directory, 'train_data_01_4_200.csv'), delimiter='$')
+train_df = pd.read_csv(os.path.join(directory, training_data_file), delimiter='$')
 
 train_df['jd_reusme'] = train_df['query'] + "\n" + train_df['resume']
 train_df = train_df.dropna(axis=0)
@@ -75,15 +89,10 @@ if tokenizer.pad_token is None:
     model.resize_token_embeddings(len(tokenizer))
     
     
-MAX_LENGTH = 128
-train_dataset = BinaryClassificationDataset(train_data, tokenizer, max_length=MAX_LENGTH)
-valid_dataset = BinaryClassificationDataset(valid_data, tokenizer, max_length=MAX_LENGTH)
+train_dataset = BinaryClassificationDataset(train_data, tokenizer, max_length=max_sql_len)
+valid_dataset = BinaryClassificationDataset(valid_data, tokenizer, max_length=max_sql_len)
     
-epochs = 1
-batch_size = 1
-lr = 2e-5
 warmup_steps = 500
-MAX_STEPS = 2
 num_training_steps = epochs * (len(train_dataset) // batch_size)
 
 train_dataloader = DataLoader(train_dataset, sampler=RandomSampler(train_dataset), batch_size=batch_size)
@@ -91,8 +100,6 @@ valid_dataloader = DataLoader(valid_dataset, batch_size=batch_size)
 
 # optimizer = torch.optim.AdamW(model.parameters(), lr=lr)
 # scheduler = get_linear_schedule_with_warmup(optimizer, num_warmup_steps=warmup_steps, num_training_steps=num_training_steps)
-
-fp16 = False if load_in_8bit else True
 
 training_args = TrainingArguments(
     output_dir=os.path.join(training_dir, './results'),
@@ -109,7 +116,7 @@ training_args = TrainingArguments(
     save_steps=500,
     fp16=fp16,
     tf32=True,
-    max_steps=MAX_STEPS,
+    max_steps=max_steps,
 )
 
 
